5a02cf959548741e16a4cf2afa00369114d96537
John Mark Adriano <j.adriano@saedubai.com>
2017-02-09 15:34:57 +0400

    Move

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,13 +4,40 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    Rigidbody rb;
+
+    public float speed = 2f;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.forward * vertical * speed * Time.deltaTime;
+        rb.MovePosition(rb.position + move);
+
+        float turn = horizontal * speed * Time.deltaTime;
+        Quaternion turnRot = Quaternion.Euler(0, turn, 0);
+
+        rb.MoveRotation(rb.rotation * turnRot);
+    }
+
 }
